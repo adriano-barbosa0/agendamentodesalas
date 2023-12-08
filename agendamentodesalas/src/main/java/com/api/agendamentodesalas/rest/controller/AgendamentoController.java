bd13957dcbfcc39a0e012e8b0b2a14766cbc45d1
adriano <adrianobarbosa3001@gmail.com>
2023-12-07 21:44:34 -0300

    -Implementacao do Swagger para documentacao da Api.

--- agendamentodesalas/src/main/java/com/api/agendamentodesalas/rest/controller/AgendamentoController.java
+++ agendamentodesalas/src/main/java/com/api/agendamentodesalas/rest/controller/AgendamentoController.java
@@ -1,4 +1,4 @@
-package com.api.agendamentodesalas.rest;
+package com.api.agendamentodesalas.rest.controller;
 
 import com.api.agendamentodesalas.converter.AgendamentoConverter;
 import com.api.agendamentodesalas.entity.AgendamentoEntity;
@@ -10,6 +10,10 @@ import com.api.agendamentodesalas.rest.request.AtualizaAgendamentoRequest;
 import com.api.agendamentodesalas.rest.request.AtualizaAgendamentoRequestItem;
 import com.api.agendamentodesalas.rest.request.CriaAgendamentoRequest;
 import com.api.agendamentodesalas.rest.response.ListaAgendamentoResponseItem;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -27,14 +31,24 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/agendamento")
 @Slf4j
-public class AgendamentoController {
+@Api(tags = "Agendamento", description = "Operações de agendamento")
+public class AgendamentoController   {
     @Autowired
     private SalaRepository salaRepository;
     @Autowired
     private AgendamentoRepository agendamentoRepository;
 
     @GetMapping("/agendados")
+    @ApiOperation(value = "Busca agendamentos de uma sala por período",
+            notes = "Retorna uma lista de agendamentos de uma sala para um intervalo de datas especificado.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Operação bem-sucedida. Retorna os agendamentos."),
+            @ApiResponse(code = 400, message = "Requisição inválida. Verifique os parâmetros da solicitação."),
+            @ApiResponse(code = 404, message = "Sala não encontrada. A sala especificada não existe."),
+            @ApiResponse(code = 500, message = "Erro interno do servidor. Entre em contato com o suporte técnico.")
+    })
     public ResponseEntity buscaAgendados(
+
             @RequestParam("dataInicio") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dataInicio,
             @RequestParam("dataFim") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dataFim,
             @RequestParam("salaId") Long salaId) {
@@ -52,6 +66,13 @@ public class AgendamentoController {
     }
 
     @GetMapping("/disponibilidade")
+    @ApiOperation(value = "Busca horários disponíveis para agendamento em uma sala por período",
+            notes = "Retorna os horários disponíveis para agendamento em uma sala para um intervalo de datas especificado.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Operação bem-sucedida. Retorna os horários disponíveis."),
+            @ApiResponse(code = 404, message = "Sala não encontrada. A sala especificada não existe."),
+            @ApiResponse(code = 500, message = "Erro interno do servidor. Entre em contato com o suporte técnico.")
+    })
     public List<LocalDateTime> buscaDisponibilidade(
             @RequestParam("dataInicio") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dataInicio,
             @RequestParam("dataFim") @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate dataFim,
@@ -81,6 +102,14 @@ public class AgendamentoController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Cria um novo agendamento para uma sala",
+            notes = "Cria um novo agendamento para uma sala em uma data e hora especificadas.")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Agendamento criado com sucesso."),
+            @ApiResponse(code = 400, message = "Solicitação inválida. Verifique os dados enviados."),
+            @ApiResponse(code = 404, message = "Sala não encontrada. A sala especificada não existe."),
+            @ApiResponse(code = 500, message = "Erro interno do servidor. Entre em contato com o suporte técnico.")
+    })
     public ResponseEntity criaAgendamento(@RequestBody CriaAgendamentoRequest criaAgendamentoRequest) {
         Optional<SalaEntity> salaEntityOptional = salaRepository.findById(criaAgendamentoRequest.getSalaId());
 
@@ -109,6 +138,12 @@ public class AgendamentoController {
     }
 
     @PutMapping
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Agendamento reagendado com sucesso."),
+            @ApiResponse(code = 400, message = "Solicitação inválida. Verifique os dados enviados."),
+            @ApiResponse(code = 404, message = "Agendamento antigo não encontrado. O agendamento informado como antigo não existe."),
+            @ApiResponse(code = 500, message = "Erro interno do servidor. Entre em contato com o suporte técnico.")
+    })
     public ResponseEntity reagendar(@RequestBody AtualizaAgendamentoRequest atualizaAgendamentoRequest) {
 
         // verifica se o antigo existe
--- agendamentodesalas/src/main/java/com/api/agendamentodesalas/rest/controller/SalaController.java
+++ agendamentodesalas/src/main/java/com/api/agendamentodesalas/rest/controller/SalaController.java
@@ -1,4 +1,4 @@
-package com.api.agendamentodesalas.rest;
+package com.api.agendamentodesalas.rest.controller;
 
 import com.api.agendamentodesalas.converter.SalaConverter;
 import com.api.agendamentodesalas.entity.SalaEntity;
@@ -6,6 +6,10 @@ import com.api.agendamentodesalas.repository.SalaRepository;
 import com.api.agendamentodesalas.rest.request.CriaSalaRequest;
 import com.api.agendamentodesalas.rest.response.BuscaSalaResponse;
 import com.api.agendamentodesalas.rest.response.CriaSalaResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +22,18 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/sala")
+@Api(tags = "Sala", description = "Operações relacionadas a salas")
 public class SalaController {
     @Autowired
     private SalaRepository salaRepository;
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Busca uma sala por ID",
+            notes = "Retorna informações de uma sala com base no ID fornecido.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Sala encontrada. Retorna os detalhes da sala."),
+            @ApiResponse(code = 404, message = "Sala não encontrada. O ID especificado não corresponde a nenhuma sala.")
+    })
     private ResponseEntity buscaSala(@PathVariable Long id) {
         Optional<SalaEntity> byId = salaRepository.findById(id);
         if (byId.isEmpty())
@@ -33,6 +44,11 @@ public class SalaController {
     }
 
     @GetMapping
+    @ApiOperation(value = "Busca todas as salas",
+            notes = "Retorna informações de todas as salas cadastradas.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Salas encontradas. Retorna a lista de todas as salas cadastradas.")
+    })
     private ResponseEntity buscaTodasAsSalas() {
         List<BuscaSalaResponse> salaResponseList = salaRepository.findAll()
                 .stream()
@@ -42,6 +58,12 @@ public class SalaController {
     }
 
     @PostMapping
+    @ApiOperation(value = "Cria uma nova sala",
+            notes = "Cria uma nova sala com base nos detalhes fornecidos.")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Sala criada com sucesso."),
+            @ApiResponse(code = 400, message = "Solicitação inválida. A sala já existe no banco de dados.")
+    })
     private ResponseEntity criaSala(@RequestBody @Valid CriaSalaRequest criaSalaRequest) {
         Optional<SalaEntity> byNomeAndLocal = salaRepository.findByNomeAndLocal(criaSalaRequest.getNome(), criaSalaRequest.getLocal());
 
